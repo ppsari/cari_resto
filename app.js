37635d862e285cef2634c25671dbace7cb60e338
raynormw <tirtawiryaputra@yahoo.com>
2017-05-24 18:47:52 +0700

    add skeleton

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const index = require('./routes/index');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', index);
+
+app.listen(3000, () => console.log("Listening on port 3000"));
