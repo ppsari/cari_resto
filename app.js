4f83f79eb4ca4bfab454ffb8fe4521aa0db5db30
raynormw <tirtawiryaputra@yahoo.com>
2017-05-25 15:33:59 +0700

    add weather feature

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const index = require('./routes/index');
+const weather = require('./routes/weather');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', index);
+app.use('/resto/', weather);
 
 app.listen(3000, () => console.log("Listening on port 3000"));
